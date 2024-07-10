d702d91ed3af58464d723b83c7721e1878defb92
johngerving <gervingjohn@gmail.com>
2024-07-09 17:44:49 -0700

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 // vite.config.js
+import tailwindcss from "tailwindcss";
+
 export default {
   // config options
   base: "/",
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 };
